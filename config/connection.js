db1624d0f0a35a6ca275626e524405477838f616
MNlegion <rkreuser30@gmail.com>
2023-09-25 19:01:50 -0400

    intial setup of USER model

--- config/connection.js
+++ config/connection.js
@@ -3,10 +3,10 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-const Sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
 });
 
-module.exports = Sequelize;
+module.exports = sequelize;
