9496da0b8cb7a5332fb0f3f62dd357f51efc6001
MNlegion <rkreuser30@gmail.com>
2023-09-30 14:38:18 -0400

    continued work on password hashing in user.js

--- models/User.js
+++ models/User.js
@@ -62,6 +62,7 @@ User.init(
     modelName: "user",
   },
   {
+  
     // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
 
     // pass in our imported sequelize connection (the direct connection to our database)
