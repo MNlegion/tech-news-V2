23f16c444d79c8a3cbde46924f2be0f774c011ec
MNlegion <rkreuser30@gmail.com>
2023-10-05 19:38:11 -0400

    continued post routing

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -21,7 +21,6 @@ router.get("/", (req, res) => {
     });
 });
 
-
 // GET Post by ID
 router.get("/:id", (req, res) => {
   Post.findOne({
@@ -50,18 +49,42 @@ router.get("/:id", (req, res) => {
 });
 
 // Create a Post
-router.post('/', (req, res) => {
-    // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
-    Post.create({
+router.post("/", (req, res) => {
+  // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
+  Post.create({
+    title: req.body.title,
+    post_url: req.body.post_url,
+    user_id: req.body.user_id,
+  })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.put("/:id", (req, res) => {
+  Post.update(
+    {
       title: req.body.title,
-      post_url: req.body.post_url,
-      user_id: req.body.user_id
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+      res.json(dbPostData);
     })
-      .then(dbPostData => res.json(dbPostData))
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
